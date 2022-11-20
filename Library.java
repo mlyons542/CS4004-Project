dd4e7d92e9ec2c5aaa1bde3e2996ff5c44e6a25e
mlyons542 <117187897+mlyons542@users.noreply.github.com>
2022-11-20 14:15:00 +0000

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,7 +1,25 @@
 public class Library {
-    
-    
+    private ArrayList<Department> departments;
+
     public Library() {
-        
+        departments = new ArrayList<Department>();
+    }
+
+    public void addDepartment(Department d) {
+        departments.add(d);
+    }
+
+    public void removeDepartment(Department d) {
+        departments.remove(d);
+    }
+
+    public String returnDepartment(Department d) {
+        Department dep = new Department();
+        for (int i = 0; i < departments.size(); i++) {
+            if(departments.get(i) == d) {
+                dep = d;
+            }
+        }
+        return dep.toString();
     }
 }
