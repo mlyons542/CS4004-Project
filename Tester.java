dcefc7e5588ae645ccc1c63eeefa7723769194f9
mlyons542 <117187897+mlyons542@users.noreply.github.com>
2022-11-20 22:13:31 +0000

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
@@ -11,12 +12,37 @@ public class Tester {
         joe.getRole();
         assertTrue((joe.getRole() == "customer"), "customer");
     }
-    
+
     @Test
     public void checkBookWasAdded() {
         Library uwon = new Library();
         Department standard = new Department("standard");
         standard.addBook(new Book("Biology Facts", "Joe Smith", "Biology"));
         assertTrue(standard.getBookTitle("Biology Facts") == "Biology Facts");
+    }
+
+    @Test
+    public void checkSub() {
+        Library uwon = new Library();
+        Department standard = new Department("standard");
+        standard.subbed(true, "Science Facts");
+        assertTrue(standard.getSub() == true);
+    }
+
+    @Test
+    public void checkNecessarySubs() {
+        Library uwon = new Library();
+        Department standard = new Department("standard");
+        standard.subbed(true, "Science Facts");
+        standard.necessarySubs();
+        assertTrue(standard.necessarySubs() == "");
+    }
+
+    @Test
+    public void checkBookOut() {
+        Library uwon = new Library();
+        Department standard = new Department("standard");
+        standard.addBook(new Book("Biology Facts", "Joe Smith", "Biology"));
+        assertTrue(standard.acquireBook("Biology Facts") != null);
     }
 }
