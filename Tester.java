c76b899ad8be5bb24dd7c389ae8cc87f567b1e0d
mlyons542 <117187897+mlyons542@users.noreply.github.com>
2022-11-20 15:51:46 +0000

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,5 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.Test;
 
 public class Tester {
-  
+    
+    @Test
+    public void checkUsername() {
+        Library uwon = new Library();
+        User joe = new User("joesmith22", "customer");
+        joe.getRole();
+        assertTrue((joe.getRole() == "customer"), "customer");
+    }
 }
