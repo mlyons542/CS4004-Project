a309d6d132e248d5a350c4ff1161e3ee8e5e7855
mlyons542 <117187897+mlyons542@users.noreply.github.com>
2022-11-20 20:19:43 +0000

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,17 +1,42 @@
 import java.util.ArrayList;
 
-public class Department {
+public class Department extends Library{
+    private String name;
     private ArrayList<Book> books;
 
-    public Department() {
+    public Department(String name) {
         books = new ArrayList<Book>();
+    }
+
+    public void addBook(Book b) {
+        books.add(b);
+    }
+
+    public String acquireBook(String b) {
+        String ans = "This book could not be found";
+        for (Book bk : books) {
+            if (bk.getTitle() == b) {
+                ans = bk.toString();
+            }
+        }
+        return ans;
+    }
+
+    public String getBookTitle(String t) {
+        String ans = "";
+        for (Book bk : books) {
+            if (bk.getTitle() == t) {
+                ans = t;
+            }
+        }
+        return ans;
     }
 
     @Override 
     public String toString() {
-        String ans = "";
+        String ans = "Name: " + this.name + "\n";
         for (Book b : books) {
-            ans = b.toString();
+            ans += b.toString();
         }
         return ans;
     }
